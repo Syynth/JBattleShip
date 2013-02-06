3907763f696975cead5532415ead797bd4d960a0
Ben Cochrane <syynth@syynth.com>
2013-02-05 22:29:53 -0500

    Began client protocol implementation.

--- src/jbattleclient/JBattleClientProtocol.java
+++ src/jbattleclient/JBattleClientProtocol.java
@@ -5,5 +5,23 @@ package jbattleclient;
  * @author Ben Cochrane
  */
 public class JBattleClientProtocol {
-
+    enum ProtocolState {
+        S_WAIT_FOR_CONNECTION,
+        C_ATTEMPT_CONNECTION,
+    }
+    
+    public JBattleClientProtocol(int mode) {
+        mMode = mode;
+        if (mode == SERVER) {
+            mState = ProtocolState.S_WAIT_FOR_CONNECTION;
+        } else if (mode == CLIENT) {
+            mState = ProtocolState.C_ATTEMPT_CONNECTION;
+        }
+    }
+    
+    private int mMode;
+    private ProtocolState mState;
+    
+    public static final int CLIENT = 0;
+    public static final int SERVER = 1;
 }
