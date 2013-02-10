bcd8f0f3ed4b1710f0787ede1f8e4a9cf0521957
Ben Cochrane <ben@ngon.cc>
2013-02-09 23:33:04 -0500

    Added Action, Move, Result, Shoot, and Radar classes.

---
+++ src/jbattle/client/Radar.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jbattle.client;
+
+/**
+ *
+ * @author Ben
+ */
+public class Radar extends Action implements Move, Result {
+    
+}
---
+++ src/jbattle/client/Shoot.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jbattle.client;
+
+/**
+ *
+ * @author Ben
+ */
+public class Shoot extends Action implements Move, Result {
+    
+}
