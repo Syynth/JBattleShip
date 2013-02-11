6dfdb75ade77221d02144131a991a54b7a34288a
Syynth <ben@ngon.cc>
2013-02-11 08:32:42 -0500

    Added an extra check to set board array to null.

--- src/jbattle/client/Board.java
+++ src/jbattle/client/Board.java
@@ -9,7 +9,11 @@ public class Board {
     
     public Board(Input i, int w, int h) {
         grid = new Entity[w][h];
-        
+        for (int x = 0; x < w; ++x) {
+            for (int y = 0; y < h; ++y) {
+                grid[x][y] = null;
+            }
+        }
     }
     
     public Result[] getResults(Move[] m) {
