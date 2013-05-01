f987ce30bd6cd4c9f75bc7b7506ce785a2acf924
Ben Cochrane <syynth@syynth.com>
2013-04-30 23:05:12 -0400

    Made the Client able to skip the location broker

--- src/jbattle/client/Client.java
+++ src/jbattle/client/Client.java
@@ -16,52 +16,62 @@ public class Client {
     
     public Client() {
         Config.LoadConfig("./resources/config.xml");
-        try {
-            mConnection = new Socket(Config.getProperty("serverAddress"), Config.getInt("serverPort"));
-            mInput = new ObjectInputStream(mConnection.getInputStream());
-            mOutput = new ObjectOutputStream(mConnection.getOutputStream());
-        } catch (IOException e) {
-            System.out.println("Unable to create connection to server.");
+        if (Config.getBoolean("useLocationBroker")) {
+            try {
+                mConnection = new Socket(Config.getProperty("serverAddress"), Config.getInt("serverPort"));
+                mInput = new ObjectInputStream(mConnection.getInputStream());
+                mOutput = new ObjectOutputStream(mConnection.getOutputStream());
+            } catch (IOException e) {
+                System.out.println("Unable to create connection to server.");
+            }
         }
-        
     }
 
     public void execute() {
-        
-        if (mInput == null || mOutput == null) {
-            System.out.println("Unable to create input/output streams.");
-            return;
-        }
-        
-        ServerProtocol protocol = new ServerProtocol(ServerProtocol.CLIENT);
-        protocol.setClientPort(Config.getInt("clientPort"));
-        writeMessage(protocol.handleMessage("begin"));
-        
         int mode = -1;
         String address = "127.0.0.1:" + Config.getProperty("clientPort");
-        
-        do {    // service loop
-            try {
-                
-                String smsg = mInput.readObject().toString();
-                String cmsg = protocol.handleMessage(smsg);
-                
-                if (cmsg.equals("accept wait:" + Config.getProperty("clientPort"))) {
-                    mode = ClientProtocol.SERVER;
-                } else if ("accept meet".equals(cmsg)) {
-                    System.out.println("server> " + smsg);
-                    address = smsg.substring(4);
-                    mode = ClientProtocol.CLIENT;
-                }
-                
-                writeMessage(cmsg);
-            } catch (ClassNotFoundException | IOException e) {}
-        } while (!protocol.quit());
-        
-        if (mode == ClientProtocol.SERVER) {
-            System.out.println("***Server mode");
+        if (Config.getBoolean("useLocationBroker")) {
+            if (mInput == null || mOutput == null) {
+                System.out.println("Unable to create input/output streams.");
+                return;
+            }
+
+            ServerProtocol protocol = new ServerProtocol(ServerProtocol.CLIENT);
+            protocol.setClientPort(Config.getInt("clientPort"));
+            writeMessage(protocol.handleMessage("begin"));
+
+            
+
+            do {    // service loop
+                try {
+
+                    String smsg = mInput.readObject().toString();
+                    String cmsg = protocol.handleMessage(smsg);
+
+                    if (cmsg.equals("accept wait:" + Config.getProperty("clientPort"))) {
+                        mode = ClientProtocol.SERVER;
+                    } else if ("accept meet".equals(cmsg)) {
+                        System.out.println("server> " + smsg);
+                        address = smsg.substring(4);
+                        mode = ClientProtocol.CLIENT;
+                    }
+
+                    writeMessage(cmsg);
+                } catch (ClassNotFoundException | IOException e) {}
+            } while (!protocol.quit());
+
+            if (mode == ClientProtocol.SERVER) {
+                System.out.println("***Server mode");
+            } else {
+                System.out.println("***Client mode");
+            }
         } else {
-            System.out.println("***Client mode");
+            address = Config.getProperty("serverAddress") + ":" + Config.getProperty("serverPort");
+            if (Config.getBoolean("defaultToClient")) {
+                mode = ClientProtocol.CLIENT;
+            } else {
+                mode = ClientProtocol.SERVER;
+            }
         }
         Net jnet = new Net(mode, address);
         Game game = new Game(jnet);
